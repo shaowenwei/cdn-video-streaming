f819ca9186bdc094616d31610fe77a57a63628ac
Shawn Wei <shawnwei@0587378366.wireless.umich.net>
2017-02-07 22:33:30 -0500

    s

--- proxy.cpp
+++ proxy.cpp
@@ -135,7 +135,6 @@ int main(int argc, char* argv[])
 						cout << "Error sending to web server" << endl;
 					}
 
-					buf[1000] = "";
 					// receive from web server
 					int bytesRecv = recv(serversd, &buf, 1000, 0);
 					if(bytesRecv > 0){
